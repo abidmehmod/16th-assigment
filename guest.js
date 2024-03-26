ccc748e88a2115cec1b5bcf260a4883aa65fa5d7
abidmehmod <160239077+abidmehmod@users.noreply.github.com>
2024-03-26 14:51:18 +0500

    new guests invite

---
+++ guest.js
@@ -0,0 +1,16 @@
+"use strict";
+let guest_list = ['Abid', 'mehmood', 'romi', 'waqas'];
+// for (let i = 0; i<guest_list.length; i++){
+//     console.log('dear Mr. ' + guest_list[i] + ',\n\nit is our pleasure to invite you in fection.\n\nthank you!' )
+// }
+let not_present = "mehmood";
+let new_guest = "hamza";
+guest_list[1] = new_guest;
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('dear Mr. ' + guest_list[i] + ',\n\nit is our pleasure to invite you in fection.\n\nthank you!\n');
+}
+guest_list.unshift('usama', 'imran', 'ameen');
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('dear Mr. ' + guest_list[i] + ',\n\nit is our pleasure to invite you in fection. we found big table so we decide to invite 3 more guest \n\nthank you!\n');
+}
+// console.log(`mr ${not_present} will not coming tomorow dinner ` );
